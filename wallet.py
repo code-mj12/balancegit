1d608c6895bcc7154fe16f1572abcff32b687169
MajidCodes <84784641+code-mj12@users.noreply.github.com>
2023-02-04 17:13:07 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -2,7 +2,7 @@ class Wallet:
   def __init__(self,balance):
     self.balance=balance
   def set_balance(self,val):
-    self.balance = val
+    self.balance = self.balance + val
   def withdraw(self, amount):
     self.balance -= amount
   def deposit(self, amount):
